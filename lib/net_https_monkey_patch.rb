de82f7f1dec570865c8bafa8212124988285b43a
Marcelo De Polli <marcelo.polli@gmail.com>
2016-02-17 13:18:46 -0200

    Add Ruby 1.8 verification for Net::HTTP monkey patch

--- lib/net_https_monkey_patch.rb
+++ lib/net_https_monkey_patch.rb
@@ -3,7 +3,9 @@ require 'net/https'
 
 module Net
   class HTTP
-    # Adds the ability to access the ssl_version of the ssl_context (already exists in Ruby 1.9.3)
-    ssl_context_accessor :ssl_version
+    if RUBY_VERSION =~ /^1.8/
+      # Adds the ability to access the ssl_version of the ssl_context (already exists in Ruby 1.9.3)
+      ssl_context_accessor :ssl_version
+    end
   end
 end
